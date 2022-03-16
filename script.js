8fc0a0c703658b42dad6e693af6b9e3d5d0ddee7
aafaydi <97416091+aafaydi@users.noreply.github.com>
2022-03-16 18:48:30 -0400

    update

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 var prtfinderkey = "li5ll4ilWR4AVVCI3TCQws8WvKagfj1q9XAzUykX36Uu57OZem"
 
-// var apiUrl = `https://pixabay.com/api/?key=25611667-83c4cb097f67d0655d4305c45&q=${entry}&image_type=photo&pretty=true`;
+
 var submitbtn = document.querySelector(".submitbtn")
 var factscontainer = document.querySelector("#facts-container")
 var picturesContainer = document.querySelector(".pictures");
+var catInfo = document.querySelector("#catinfo");
+
+getcatinfo();
+
 
 var dropmenu = document.getElementById("breed")
 submitbtn.addEventListener("click", function () {
-    // console.log(dropmenu.value)
-    picturesContainer.innerHTML = "";
     factscontainer.innerHTML = "";
     getdata();
   
-    // var name = document.createElement("h2")
-    // name.textContent = dropmenu.value
-    // factscontainer.appendChild(name)
-})
+   })
 
 
 function getdata() {
@@ -26,15 +25,14 @@ function getdata() {
             if (response.ok) {
                 response.json()
                     .then(function (data) {
-                        // console.log(data)
-                        // console.log(data[0].description);
+                       
                         var catdes = data[0].description;
                         var catdes1 = document.createElement("div");
 
                         catdes1.textContent = catdes;
                         factscontainer.appendChild(catdes1);
-                        displayImage(data[0].reference_image_id);
-
+                        displayImage(data[0].reference_image_id,data[0].name);
+                        
 
 
                     })
@@ -44,12 +42,21 @@ function getdata() {
 }
 
 
-function displayImage(imageId) {
+function displayImage(imageId,catname) {
     
     //1. use imageId to construct an image url for our API
     var url = "https://cdn2.thecatapi.com/images/" + imageId + ".jpg";
-    //var url = `"https://cdn2.thecatapi.com/images/${imageId}.jpg`;
+    
+
+    console.log(imageId);
+    
     console.log(url);
+    
+    document.getElementById("CatPhoto").src = url;
+    document.getElementById("CatPhoto").alt = "photo of a " + catname;
+
+
+    // picturesContainer.appendChild(image);
     
     //2. create an image element
 
@@ -58,9 +65,29 @@ function displayImage(imageId) {
     //4. append our brand-spankin-new image element to our 'pictures' element
 
 
+    }
 
+    function getcatinfo(){
+        var baseurl = `https://cat-fact.herokuapp.com/facts/random`
+        fetch(baseurl)
+            .then(function (response) {
+                if (response.ok) {
+                    response.json()
+                        .then(function (data) {
+                            console.log(data)
+                            var catInfo1 = data.text;
+                            var catInfo2 = document.createElement("p");
+    
+                            catInfo2.textContent = catInfo1;
+                            catInfo.appendChild(catInfo2);
+                            
+    
+    
+                        })
+                }
+    
+            })
+        }
+    
 
-
-
-
-}
+    
