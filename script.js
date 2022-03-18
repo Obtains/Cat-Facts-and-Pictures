ccc4e4e7177d878e6d81b1916f422bc5a3fb37bc
aafaydi <97416091+aafaydi@users.noreply.github.com>
2022-03-18 10:08:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var prtfinderkey = "li5ll4ilWR4AVVCI3TCQws8WvKagfj1q9XAzUykX36Uu57OZem"
 
-
+// connects the classes to variables for access 
 var submitbtn = document.querySelector(".submitbtn")
 var factscontainer = document.querySelector("#facts-container")
 var picturesContainer = document.querySelector(".pictures");
@@ -12,8 +12,8 @@ var phone = document.getElementById("phone")
 var submitform = document.querySelector("#formsubmit");
 
 
-
- submitform.addEventListener("click", function (event) {
+// add event listener waiting for user click on first submit button
+submitform.addEventListener("click", function (event) {
     event.preventDefault();
     console.log(inputname.value);
     console.log(email.value);
@@ -40,11 +40,11 @@ var submitform = document.querySelector("#formsubmit");
 
 
 
-
+// call the first Api
 getcatinfo();
 
 
-
+// add event listener waiting for user click on second submit button
 var dropmenu = document.getElementById("breed")
 submitbtn.addEventListener("click", function () {
     factscontainer.innerHTML = "";
@@ -52,9 +52,10 @@ submitbtn.addEventListener("click", function () {
 
 })
 
-
+// access the second Api date uing fetch then get the info we need from it
 function getdata() {
 
+    // get all data
     var baseurl = `https://api.thecatapi.com/v1/breeds/search?q=${dropmenu.value}`
     fetch(baseurl)
         .then(function (response) {
@@ -62,11 +63,16 @@ function getdata() {
                 response.json()
                     .then(function (data) {
 
+                        //get the description element from data array index 0                         
                         var catdes = data[0].description;
+                        // create div and assign it to catdes1                        
                         var catdes1 = document.createElement("div");
+                        //put the cat descrip in the div                        
                         // sendEmail(dropmenu.value, catdes); 
                         catdes1.textContent = catdes;
+                        // then display it on the second block which is fastscontainer                        
                         factscontainer.appendChild(catdes1);
+                        // also from these info call display image and display the image
                         displayImage(data[0].reference_image_id, data[0].name);
 
 
@@ -77,32 +83,20 @@ function getdata() {
         })
 }
 
-
+// display cats imahgeson the third block
 function displayImage(imageId, catname) {
 
     //1. use imageId to construct an image url for our API
     var url = "https://cdn2.thecatapi.com/images/" + imageId + ".jpg";
 
-
-    console.log(imageId);
-
-    console.log(url);
-
+    //add our url to the image element as the 'src' property (BONUS - add an "alt" tag)
     document.getElementById("CatPhoto").src = url;
     document.getElementById("CatPhoto").alt = "photo of a " + catname;
 
 
-    // picturesContainer.appendChild(image);
-
-    //2. create an image element
-
-    //3. add our url to the image element as the 'src' property (BONUS - add an "alt" tag)
-
-    //4. append our brand-spankin-new image element to our 'pictures' element
-
-
 }
 
+// The first Api about all cats general facts 
 function getcatinfo() {
     var baseurl = `https://cat-fact.herokuapp.com/facts/random`
     fetch(baseurl)
@@ -110,11 +104,14 @@ function getcatinfo() {
             if (response.ok) {
                 response.json()
                     .then(function (data) {
-                        console.log(data)
-                        var catInfo1 = data.text;
-                        var catInfo2 = document.createElement("p");
 
+                        //get the fact from data,.text andd assign it to catinfo1
+                        var catInfo1 = data.text;
+                        // create paragraph and assing it to catinfo2
+                        var catInfo2 = document.createElement("p");
+                        // put the field of text into the catinfo2
                         catInfo2.textContent = catInfo1;
+                        //display it on the page
                         catInfo.appendChild(catInfo2);
 
 
@@ -124,5 +121,3 @@ function getcatinfo() {
 
         })
 }
-
-
